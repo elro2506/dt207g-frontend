2b0d848780e536a555862184475d56bf23c47b5c
Elin Ronda <elro2506@student.miun.se>
2026-04-23 15:07:25 +0200

    Fixat javascriptkod i main och add. Testkört i liveserver

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-fetch("http://localhost:5000/experience")
-.then(res => res.json())
-.then(data => {
+const url = "http://localhost:5000/experience";
+
+function getData() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => writeExperience(data))
+    .catch(err => console.log("Error" + err));
+}
+
+//Skriver ut data
+function writeExperience(data) {
     const list = document.getElementById("list");
+    list.innerHTML = "";
 
     data.forEach(item => {
         const li = document.createElement("li");
@@ -11,15 +20,40 @@ fetch("http://localhost:5000/experience")
         const button = document.createElement("button");
         button.textContent = "Radera";
 
-        button.onclick = () => {
-            fetch("http://localhost:5000/experience/" + item.id, {
-                method: "DELETE"
-            })
-
-            .then(() => location.reload());
-        };
-
+        button.onclick = () => deleteExperience(item.id);
+        
         li.appendChild(button);
         list.appendChild(li);
     });
-});
+}
+
+function deleteExperience(id) {
+    fetch(`${url}/${id}`, {
+        method: "DELETE"
+    })
+.then(() => getData())
+.catch(err => console.log("Error" + err));
+}
+
+async function sendApiRequest(data) {
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify(data)
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.log("Error" + error);
+    }
+}
+
+getData();
